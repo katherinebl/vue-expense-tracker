2cfde2d3aca7c69010a3f298670da764527ba19f
katherine.briceno <kath.gbl@gmail.com>
2024-03-09 22:02:16 +0100

    Dynamic transactions using the Options API

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,27 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Course <span>$200</span><button class="delete-btn">x</button>
+        <li 
+            v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }} <span>€{{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                transactions: [
+                    { id: 1, text: 'Book', amount: -20 },
+                    { id: 2, text: 'Salary', amount: 500 },
+                    { id: 3, text: 'Tickets', amount: -208 },
+                    { id: 4, text: 'Camera', amount: 130 },
+                ]
+            }
+        }
+    }
+</script>
